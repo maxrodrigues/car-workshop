3df9cea7c1cff4d509d97f6ffcb738f48fa90862
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-03-28 19:23:07 -0300

    change view page

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Welcome')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome')
+
+{{-- Content body: main page content --}}
+
+@section('content_body')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
